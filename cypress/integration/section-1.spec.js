0885d0f4c55b64778f9877b2d3df4272c6e19ea6
SMehraGit <mehra.sam2501@gmail.com>
2021-08-02 18:50:24 +1000

    Alaya care automation challenge

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,71 @@
 const { Section1 } = require('../objects/section-1')
 
 describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+    cy.visit('http://192.168.1.107:8080/section-1')
+  })
+
+  it('DOM Tables', () => {
+    // Test1 - Assert that the table is not visible
+    cy.get('[id=alaya-table]').should('not.be.visible')
+
+    // Test2 - After clicking the "Show table" button, assert the table is visible
+    cy.get('[id=table-toggle-button]').click()
+    cy.get('[id=alaya-table]').should('be.visible')
+
+    // Test3 - Assert that the table is 5 columns wide
+    cy.get('[data-test=table-header]').find('th').should('have.length', 5)
+
+    // Test4 - Assert that the table is 10 rows long, excluding the first (header) row
+    cy.get('[id=alaya-table]').find('tr').eq(10)
+    const givenRowLength = 10
+
+    cy.get('[id=alaya-table]').find('tr').should('have.length', (givenRowLength + 1))
+
+    // Test5 - Assert that at least 5 entries have the role user
+    let count = 0
+
+    cy.get('[id=alaya-table]')
+      .find('tr')
+      .then((rows) => {
+        rows.toArray().forEach((element) => {
+          if (element.innerHTML.includes('user')) {
+            count += 1
+          }
+        })
+
+        cy.log(count)
+        expect(count).to.be.at.least(5)
+      })
+  })
+
+  it('DOM Forms', () => {
+    // Test1 - Assert that the form is not visible
+    cy.get('[id=alaya-form]').should('not.be.visible')
+
+    // Test2 - After clicking the "Show form" button, assert that the form is visible
+    cy.get('[id=form-toggle-button]').click()
+    cy.get('[id=alaya-form]').should('be.visible')
+
+    // Test3 - Fill in the "Name" and "Age" inputs, and assert that both inputs are filled
+    cy.get('[id=fullName]').type('Sam')
+    cy.get('[id=fullName]').should('have.value', 'Sam')
+    cy.get('[id=age]').type('26')
+    cy.get('[id=age]').should('have.value', '26')
+
+    // Test4 - Select "Female" from the select option, and assert that the value is "female"
+    cy.get('[id=gender]').select('Female')
+    cy.get('[id=gender]').should('have.value', 'female')
+
+    // Test5 - Tick the "Nurse" checkbox and assert that the value "nurse" is true
+    cy.get('[id=nurse]').check()
+    cy.get('[id=nurse]').should('be.checked')
+
+    // Test6 - Click on the "Submit" button and assert that there is an alert window showing with the text "Form submitted!"
+    cy.get('[id=submit]').click()
+    cy.on('window:alert', (alertBox) => {
+      expect(alertBox).to.contains('Form submitted!')
+    })
+  })
 })
--- cypress/integration/section-2.spec.js
+++ cypress/integration/section-2.spec.js
@@ -1,10 +1,29 @@
 const { Section2 } = require('../objects/section-2')
 
 describe('Problem 2', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section2, you can do: Section2.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+    cy.visit('http://192.168.1.107:8080/section-2')
+  })
+
+  it('Waiting for Network Calls', () => {
+    cy.get('[id=network-call-button]').click()
+    cy.wait(11000)
+    cy.request('http://localhost:8889/todos/1').then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response).to.have.property('body')
+      expect(response).to.have.property('headers')
+      expect(response).to.have.property('duration')
+      expect(response).to.have.property('status')
+    })
+
+    cy.on('window:alert', (alertBox) => {
+      expect(alertBox).to.contains('Abnormally long network call!')
+    })
+  })
+
+  it('Browser API: Opening a new tab', () => {
+    cy.get('[id=new-tab-button]').click()
+    cy.url().should('include', 'http://192.168.1.107:8080/')
+  })
 })
